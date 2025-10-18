55df065703ef22659da503e6f3dc1d0b5a5a46fb
YuR1zZ <panahim257@gmail.com>
2025-10-18 19:54:16 +0330

    landing page almost complited

--- app/components/ui/Magnet.jsx
+++ app/components/ui/Magnet.jsx
@@ -1,17 +1,53 @@
 'use client'
 
 import gsap from "gsap"
-import React , { useRef } from "react"
+import React , { useRef, useEffect} from "react"
 
 
-const Magnet = () => {
+const Magnet = ({children}) => {
 
     const ref = useRef(null)
 
+
+
+    useEffect(() => {
+
+      if (!ref.current) return;
+
+      const xTo = gsap.quickTo(ref.current, 'x', {duration:1, ease:'elastic.out(1,0.3)'})
+      const yTo = gsap.quickTo(ref.current, 'y', {duration:1, ease:'elastic.out(1,0.3)'})
+
+      const mouseMove = (e)=>{
+        const {clientX,clientY}= e;
+        const {width,height,left,top} = ref.current.getBoundingClientRect();
+        const x = clientX - (left + width / 2);
+        const y = clientY - (top + height / 2);
+        xTo(x)
+        yTo(y)
+
+      }
+
+      const mouseLeave = (e)=>{
+        xTo(0)
+        yTo(0)
+      }
+
+
+      ref.current.addEventListener('mousemove',mouseMove)
+      ref.current.addEventListener('mouseleave',mouseLeave)
+
+      return()=>{
+        if (ref.current) {
+      ref.current.removeEventListener('mousemove', mouseMove);
+      ref.current.removeEventListener('mouseleave', mouseLeave);
+    }
+      }
+    }, []);
+
+
+
   return (
-    <div ref={ref}>
-      React.cloneElement(children,{ref})
-    </div>
+    React.cloneElement(children,{ref})
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,59 @@
 'use client'
-import Link from "next/link";
+import { useEffect,useState } from "react";
 import { useRouter } from "next/navigation";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import Magnet from "./components/ui/Magnet";
+import AnimatedArrow from "./components/ui/Arrow";
+import AnimateLeftdArrow from "./components/ui/LeftArrow";
 
 
 export default function Home() {
 
+
   const router = useRouter();
+
+
+  useEffect(() => {
+    const welcomeSplit = new SplitText(".welcome", { type: "chars, words" });
+    gsap.from(welcomeSplit.chars, {
+      y:-20,
+      yoyo:true,
+      duration:1,
+    })
+  }, []);
+
+
+  useEffect(() => {
+    const pathSplit = new SplitText(".path", { type: "chars, words" });
+    gsap.from(pathSplit.words, {
+      y:30,
+      yoyo:true,
+      duration:1,
+    });
+  }, []);
+
+
+  useGSAP(()=>{
+    gsap.fromTo('.gaming-btn',{
+      opacity:0,
+      x:-30,
+    },{
+      opacity:1,
+      x:0,
+      duration:1,
+    })
+    gsap.fromTo('.coding-btn',{
+      opacity:0,
+      x:30,
+    },{
+      opacity:1,
+      x:0,
+      duration:1,
+    })
+  },[])
+
 
   return (
     <main>
@@ -13,31 +61,40 @@ export default function Home() {
 
     <div className="absolute flex flex-col items-center justify-center w-[100vw] h-[100vh] z-100 top-0 left-0 text-xl">
 
-    <div className="text-5xl mb-4">
+    <div className="text-5xl mb-4 welcome">
     <h1>Welcome</h1>
     </div>
-    <div className="mb-4 text-3xl">
-    <p className="">Please Choose Your Path</p>
+    <div className="mb-4 text-3xl path">
+    <p>Please Choose Your Path</p>
     </div>
 
-      <div className="flex flex-row gap-8 mt-10">
+      <div className="flex flex-row gap-8 mt-15">
 
+      <Magnet>
+        <div className="gaming-btn flex flex-row items-center justify-center">
+        <AnimateLeftdArrow />
       <button
        className="cursor-pointer"
        onClick={()=>router.push('/gaming')}
        >
-        <p
-        className="gradient-text">Gaming</p>
+        <p className="gaming-text">Gaming</p>
       </button>
+      </div>
+      </Magnet>
     
+      <Magnet>
+      <div className="coding-btn flex flex-row items-center justify-center">
       <button
-      className="cursor-pointer"
+      className="cursor-pointer "
       onClick={()=>router.push('/coding')}
       >
-        <p className="bg-gradient-to-b from-gray-600 via-gray-300 to-white bg-clip-text text-transparent">
+        <p className="coding-text">
           Coding
         </p>
       </button>
+      <AnimatedArrow />
+      </div>
+      </Magnet>
 
       </div>
 
