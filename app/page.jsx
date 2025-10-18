042b14d2752e367754b524683d3dd2a35bf8a3a8
YuR1zZ <panahim257@gmail.com>
2025-10-18 19:57:05 +0330

    landing page almost completed2

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect,useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
