085ff3ed8214aa359beb40a41d9a198546f7db32
YuR1zZ <panahim257@gmail.com>
2025-10-30 18:50:27 +0330

    colors changed

--- app/components/CodingIntroduction.jsx
+++ app/components/CodingIntroduction.jsx
@@ -23,7 +23,7 @@ const Introduction = () => {
         }}
         transition={{ type: "tween", ease: "backOut", duration:0.5}}
       >
-          <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
+          <p className={`${isHovered ? 'text-black' : 'text-[#afa18f]'}`} onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
             A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
           </p>
       </motion.div>
