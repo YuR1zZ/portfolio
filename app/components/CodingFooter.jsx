f42e5516a9d1bc89a4bd890bdfd057d2320e425a
YuR1zZ <panahim257@gmail.com>
2025-11-10 14:46:04 +0330

    footer line added

--- app/components/CodingFooter.jsx
+++ app/components/CodingFooter.jsx
@@ -1,17 +1,83 @@
 'use client'
 
 import SpotLight from "./ui/SpotLight"
+import { useRef , useEffect } from "react"
 
 const CodingFooter = () => {
+
+  const path = useRef(null);
+  let progress = 0;
+  let x = 0.5;
+  let time = Math.PI / 2;
+  let reqId = null;
+
+  useEffect(() => {
+    setPath(progress);
+  }, [])
+
+  const setPath = (progress) => {
+    const width = window.innerWidth * 0.7;
+    path.current.setAttributeNS(null, "d", `M0 250 Q${width * x} ${250 + progress}, ${width} 250`)
+  }
+
+  const lerp = (x, y, a) => x * (1 - a) + y * a
+
+  const manageMouseEnter = () => {
+    if(reqId){
+      cancelAnimationFrame(reqId)
+      resetAnimation()
+    }
+  }
+
+  const manageMouseMove = (e) => {
+    const { movementY, clientX } = e;
+    const pathBound =  path.current.getBoundingClientRect();
+    x = (clientX - pathBound.left) / pathBound.width;
+    progress+= movementY
+    setPath(progress);
+  }
+
+  const manageMouseLeave = () => {
+    animateOut();
+  }
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    progress = lerp(progress, 0, 0.025);
+    time+=0.2;
+    setPath(newProgress);
+    if(Math.abs(progress) > 0.75){
+      reqId = requestAnimationFrame(animateOut);
+    }
+    else{
+      resetAnimation();
+    }
+  }
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+  }
+
   return (
-    <main className='flex h-screen justify-center items-center relative'>
+    <main className='flex h-screen w-screen justify-center items-center relative'>
         <SpotLight
         lights={[
         { color: "gray", position: "top-right" },
         ]}
       />
 
-      <h2 className='text-[#EEEEEE] text-5xl'>Footer</h2>
+      <div className='line-container'>
+        <div className='line-body'>
+            <div className='line'>
+              <div onMouseEnter={() => {manageMouseEnter()}} onMouseMove={(e) => {manageMouseMove(e)}} onMouseLeave={() => {manageMouseLeave()}} className='line-box'></div>
+              <svg>
+                <path ref={path}></path>
+              </svg>
+            </div>
+        </div>
+    </div>
+
     </main>
   )
 }
