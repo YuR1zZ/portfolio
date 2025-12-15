0966022c9d22395a086858d62fc29dbbe9358e9a
YuR1zZ <panahim257@gmail.com>
2025-12-15 18:22:54 +0330

    mates component added

--- app/gaming/mates/page.jsx
+++ app/gaming/mates/page.jsx
@@ -1,10 +1,336 @@
-import React from 'react'
+'use client'
+
+import gsap from "gsap"
+import { SplitText } from "gsap/SplitText"
+import slides from '../../../constants/index.js'
+import { useEffect } from "react"
+
 
 const page = () => {
+
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', ()=>{
+      const totalSlides = slides.length;
+      let currentSlide = 1;
+
+      let isAnimating = false
+      let scrollAllowed = true
+      let lastScrollTime = 0
+
+      const createSlide = (slideIndex)=>{
+        const slideData = slides[slideIndex - 1]
+
+        const slide = document.createElement('div')
+        slide.className = 'slide'
+
+        const slideImg = document.createElement('div')
+        slideImg.className = 'slide-img'
+        const img = document.createElement('img')
+        img.src= slideData.slideImg
+        img.alt = '';
+        slideImg.appendChild(img);
+
+        const slideHeader = document.createElement('div')
+        slide.className = 'slide-header'
+
+        const slideTitle = document.createElement('div')
+        slideTitle.className = 'slide-title'
+        const h1 = document.createElement('h1')
+        h1.textContent = slideData.slideTitle
+        slideTitle.appendChild(h1);
+
+        const slideDescription = document.createElement('div')
+        slideDescription.className = 'slide-description'
+        const p = document.createElement('p')
+        p.textContent = slideData.slideDescription
+        slideDescription.appendChild(p);
+
+        const slideLink = document.createElement('div')
+        slideLink.className = 'slide-link'
+        const a = document.createElement('a')
+        a.href = slideData.slideUrl
+        a.textContent = 'View Project'
+        slideLink.appendChild(a);
+
+        slideHeader.appendChild(slideTitle);
+        slideHeader.appendChild(slideDescription);
+        slideHeader.appendChild(slideLink);
+
+        const slideInfo = document.createElement('div')
+        slideInfo.className = 'slide-info'
+
+        const slideTags = document.createElement('div')
+        slideTags.className = 'slide-tags'
+        const tagsLabel = document.createElement('p')
+        tagsLabel.textContent = 'Tags'
+        slideTags.appendChild(tagsLabel);
+
+        slideData.slideTags.forEach((tag) => {
+          const tagP = document.createElement('p');
+          tagP.textContent = tag
+          slideTags.appendChild(tagP)
+        })
+
+        const slideIndexWrapper = document.createElement('div')
+        slideIndexWrapper.className = 'slide-index-wrapper'
+        const slideIndexCopy = document.createElement('p')
+        slideIndexCopy.textContent = slideIndex.toString().padStart(2, '0')
+        const slideIndexSeperator = document.createElement('p')
+        slideIndexSeperator.textContent = '/'
+        const slidesTotalCount = document.createElement('p')
+        slidesTotalCount.textContent = totalSlides.toString().padStart(2,'0')
+        
+        slideIndexWrapper.appendChild(slideIndexCopy)
+        slideIndexWrapper.appendChild(slideIndexSeperator)
+        slideIndexWrapper.appendChild(slidesTotalCount)
+
+        slideInfo.appendChild(slideTags)
+        slideInfo.appendChild(slideIndexWrapper)
+
+        slide.appendChild(slideImg)
+        slide.appendChild(slideHeader)
+        slide.appendChild(slideInfo)
+
+        return slide;
+      }
+
+      const slitpText = (slide)=> {
+    const slideHeader = slide.querySelector('.slide-title h1')
+    if(slideHeader) {
+      SplitText.create(slideHeader, {
+        type:'word',
+        wordsClass: 'word',
+        mask:'words',
+      })
+    }
+
+    const slideContent = slide.querySelector('p,a')
+    slideContent.forEach((element)=>{
+      SplitText.create(element,{
+        type:'lines',
+        linesClass: 'line',
+        mask:'lines',
+        reduceWhiteSpace:false,
+      })
+    })
+  }
+
+  const animateSlide = (direction)=> {
+    if(isAnimating || !scrollAllowed) return
+
+    isAnimating = true
+    scrollAllowed = false
+
+    const slider = document.querySelector('.slider')
+    const currentSlideElement = slider.querySelector('.slide')
+
+    if(direction === 'down') {
+      currentSlide = createSlide === totalSlides ? 1 : currentSlide + 1
+    } else {
+      currentSlide = currentSlide === 1 ? totalSlides : currentSlide - 1
+    }
+
+    const exitY = direction === 'down' ? '-200vh' : '200vh'
+    const entryY = direction === 'down' ? '100vh' : '-100vh'
+    const entryClipPath = direction === 'down' ? 'polygon(20% 20%, 80% 20%,80% 100%,20% 100%)' : 'polygon(20% 0%, 80% 0%, 80% 80%,20% 80%)'
+
+    gsap.to(currentSlideElement, {
+      scale: 0.25,
+      opacity: 0,
+      rotation:30,
+      y:exitY,
+      duration:2,
+      ease:'power4.inOut',
+      force3d: true,
+      onComplete: ()=> {
+        currentSlideElement.remove()
+      }
+    })
+
+    setTimeout(()=>{
+      const newSlide = createSlide(currentSlide)
+
+      gsap.set(newSlide, {
+        y:entryY,
+        clipPath:entryClipPath,
+        force3d:true,
+      })
+
+      slider.appendChild(newSlide);
+
+      SplitText(newSlide);
+
+      const words = newSlide.querySelector('.word')
+      const lines = newSlide.querySelector('.line')
+
+      gsap.set([...words,...lines], {
+        y:'100%',
+        force3d:true,
+      })
+
+      gsap.to(newSlide, {
+        y:0,
+        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+        duration:1.5,
+        ease:'power4.out',
+        force3d:true,
+        onStart: ()=> {
+          const tl = gsap.timeline()
+
+          const headerWords = newSlide.querySelectorAll('.slide-title .word')
+          tl.to(
+            headerWords,
+            {
+              y:'0%',
+              duration:1,
+              ease: 'power4.out',
+              stagger:0.1,
+              force3d:true,
+            },
+            0.75
+          )
+
+          const tagsLines = newSlide.querySelectorAll('.slide-tags .line')
+          const indexLines = newSlide.querySelectorAll('.slide-index-wrapper .line')
+          const descriptionLines = newSlide.querySelectorAll('.slide-description .line')
+
+          tl.to(
+            tagsLines,
+            {
+              y:'0%',
+              duration:1,
+              ease:'power4.out',
+              stagger:0.1,
+            },
+            '-=0.75'
+          );
+
+          tl.to(
+            indexLines,
+            {
+              y:'0%',
+              duration:1,
+              ease:'power4.out',
+              stagger:0.1,
+            },
+            '<'
+          );
+
+          tl.to(
+            descriptionLines,
+            {
+              y:'0%',
+              duration:1,
+              ease:'power4.out',
+              stagger:0.1,
+            },
+            '<'
+          );
+
+          const linkLines = newSlide.querySelectorAll('.slide-link .line')
+          tl.to(
+            linkLines,
+            {
+              y:'0%',
+              duration:1,
+              ease:'power4.out',
+            },
+            '-=1'
+          )
+        },
+
+        onComplete: ()=> {
+          isAnimating = false;
+          setTimeout(()=> {
+            scrollAllowed = true;
+            lastScrollTime = Date.now()
+          },100)
+        }
+      })
+    }, 750)
+  }
+
+  const handleScroll = (direction)=>{
+    const now = Date.now();
+
+    if(isAnimating || !scrollAllowed) return
+    if(now - lastScrollTime < 1000) return
+
+    lastScrollTime = now
+    animateSlide(direction)
+  }
+
+  window.addEventListener('wheel',
+    (e)=>{
+      e.preventDefault();
+      const direction = e.deltaY > 0 ? 'down' : 'up'
+      handleScroll(direction)
+    },
+    {
+      passive:false
+    }
+  );
+
+  let touchStartY = 0
+  let isTouchActive = false
+
+  window.addEventListener('touchstart', (e)=>{
+    touchStartY = e.touches[0].clientY
+    isTouchActive = true
+  }, {passive: false})
+
+  window.addEventListener('touchmove', (e)=>{
+    e.preventDefault()
+    if(!isTouchActive || isAnimating || !scrollAllowed) return
+
+    const touchCurrentY = e.touches[0].clientY;
+    const difference = touchStartY - touchCurrentY
+
+    if(Math.abs(difference) > 50) {
+      isTouchActive = false
+      const direction = difference > 0 ? 'down' : 'up'
+      handleScroll(direction)
+    }
+  }, {passive : false})
+
+  window.addEventListener('touchend',()=>{
+    isTouchActive = false
+  })
+    })
+  }, []);
+
+  
+
+
   return (
-    <div className='h-screen text-[10rem] text-amber-50 flex justify-center items-center'>
-      <h1>Mates</h1>
-    </div>
+    <main>
+      
+      <div className='slider'>
+        <div className="slide">
+          <div className="slide-header">
+            <div className="slide-img"></div>
+            <div className="slide-title">
+              <h1>Emad Soltani</h1>
+            </div>
+            <div className="slide-description">
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias omnis voluptate minima quasi reiciendis pariatur rerum nihil necessitatibus praesentium maiores fugit ut beatae aperiam tenetur, odio repellat est. Vitae numquam accusantium quia? Sunt voluptate esse natus placeat deleniti corrupti error.
+              </p>
+            </div>
+            <div className="slide-link">
+              <a href='https://steamcommunity.com/id/Iam_MAGNET' target='_blank'>Steam Profile</a>
+            </div>
+          </div>
+          <div className="slide-info">
+            <div className="slide-info-wrapper flex-row flex">
+              <p className='slide-index'>01</p>
+              <p>/</p>
+              <p className='total-slide-count'>05</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
---
+++ constants/index.js
@@ -0,0 +1,27 @@
+const slides = [
+    {
+        slideTitle: 'Emad soltani',
+        slideDescription:"bro is the best rifler i've ever seen (for enemy team).when bro spray i can hear the birds scream. ngl he's good with AWP like shoot close with awp (misses) and long with pistol(also miss). oh man i love playing with him especially when we're in maps except dd2 and mirage. only good thing about bro is his utility usage(0% use - 100% spend money on utils)",
+        slideUrl:'',
+    },
+    {
+        slideTitle: 'Majid shokri',
+        slideDescription:"bro is literally playing for other team. mouse in hand but locking in in cooldown , Throwing matches like a ball except no one's playing with you after. most random thing is that he's planting opium.",
+        slideUrl:'',
+    },
+    {
+        slideTitle: 'Saman jamshidi',
+        slideDescription:"They say money can't buy happiness, but it can buy a rtx 4090. in some cases we had people in the hood who workin' in snapp for 1$ per trip while this mf earn 100$ while sleeping. god says life's fair. so who am i to judge.",
+        slideUrl:'',
+    },
+    {
+        slideTitle: 'Mohammad shalikari',
+        slideDescription:"no matters how or why, he's gonna find something to yap about. proof that size doesnt matter… especially when it comes to being annoying. All bark, no bite — unless its your ankle and you're wearing flip-flops.",
+        slideUrl:'',
+    },
+    {
+        slideTitle: 'Parsa bakhtiari',
+        slideDescription:"Bro just Run and shoot like a maniac. bro's hobby is stealing people's kill (Wow, look who showed up just in time… after we did the hard work.). bro either run and kill enemies and cheers about that or dies and start yelling at us, where are you guys, why you dont help. bro spend shit tons of money to migrate so he can play games with low latency, bro need to touch some grass ASAP. bro is smart but in a bad way.",
+        slideUrl:'',
+    }
+]
