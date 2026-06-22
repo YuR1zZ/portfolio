1f9d8d1ac7afc84bc949967cbcf786caeaf0a27d
YuR1zZ <panahim257@gmail.com>
2026-06-22 13:44:41 +0330

    site title and logo fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Afacad_Flux } from 'next/font/google';
 import "./globals.css";
 import Script from 'next/script';
@@ -10,21 +8,28 @@ const afacadFlux = Afacad_Flux({
   display: 'swap',
 });
 
+export const metadata = {
+  title: "Mohammad Panahi | Portfolio",
+  description: "Personal portfolio of Mohammad Panahi",
+  icons: {
+    icon: "/images/yuri.png",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <body className={`${afacadFlux.variable} antialiased`}>
+        {children}
+
         <Script
           type="module"
           src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-        ></Script>
+        />
         <Script
           nomodule
           src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-        ></Script>
-      </head>
-      <body className={`${afacadFlux.variable} antialiased`}>
-        {children}
+        />
       </body>
     </html>
   );
